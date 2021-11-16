683514f560e0da25321a37ba8e0099ae58d49955
James Crawford <jamescody@me.com>
2021-11-16 10:11:15 -0800

    Completed Prep-Javascript-Arrays.

---
+++ prep-javascript-arrays/main.js
@@ -0,0 +1,14 @@
+var languages = ['html', 'css', 'javascript', 'react', 'node', 'postgres'];
+console.log(languages);
+
+var firstElement = 'html';
+console.log('the frst element of the array is ' + firstElement);
+
+var thirdElement = 'javascript';
+console.log('the third element of the array is ' + thirdElement);
+
+var length = languages.length;
+console.log('The length of the array is ' + length);
+
+var lastElement = 'postgres';
+console.log('the last element of the array is ' + lastElement);
