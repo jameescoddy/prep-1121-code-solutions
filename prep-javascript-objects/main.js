a998aa1a2c80c7556ab07e7d630dd110a8a1ff7a
James Crawford <jamescody@me.com>
2021-11-15 20:54:10 -0800

    Completed Prep-Javascript-Objects.

---
+++ prep-javascript-objects/main.js
@@ -0,0 +1,19 @@
+var person = {
+  firstName: 'Cody',
+  lastName: 'Crawford',
+  hobby: 'Video Games'
+};
+console.log(person);
+
+var firstName = 'Cody';
+var lastName = 'Crawford';
+var fullName = firstName + ' ' + lastName;
+console.log("The person's name is:", fullName);
+
+person.job = 'Superstar';
+console.log("This person's current job is:", person.job);
+
+person.previousJob = 'Ex-Race Car Driver';
+console.log("This person's previous job is:", person.previousJob);
+
+console.log(person);
