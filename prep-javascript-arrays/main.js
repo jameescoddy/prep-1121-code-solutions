26f6a7a1fcae392e6882ba0615579e8c43985871
James Crawford <jamescody@me.com>
2021-11-16 13:39:15 -0800

    Changed languages array element.

--- prep-javascript-arrays/main.js
+++ prep-javascript-arrays/main.js
@@ -1,14 +1,14 @@
 var languages = ['html', 'css', 'javascript', 'react', 'node', 'postgres'];
 console.log(languages);
 
-var firstElement = 'html';
+var firstElement = languages[0];
 console.log('the frst element of the array is ' + firstElement);
 
-var thirdElement = 'javascript';
+var thirdElement = languages[3];
 console.log('the third element of the array is ' + thirdElement);
 
 var length = languages.length;
 console.log('The length of the array is ' + length);
 
-var lastElement = 'postgres';
+var lastElement = languages[5];
 console.log('the last element of the array is ' + lastElement);
